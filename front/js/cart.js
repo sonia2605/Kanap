0ed069728b4f9473611f6ab88a9c030b78213a0a
Sonia <sonia.livet@orange.fr>
2022-01-04 21:53:25 +0100

    page panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -25,7 +25,7 @@ productDivImg.className = "cart_item_img";
 let productImg = document.createElement ("img");
 productDivImg.appendChild (productImg);
 productImg.src = productLocalStorage [produit] .imgProduct;
-productImg.alt = productLocalStorage [produit] . altImgProduct;
+productImg.alt = productLocalStorage [produit] .altImgProduct;
 
 // élément "div" 
 let productItemContent = document.createElement ("div");
@@ -133,16 +133,190 @@ function changeQtt () {
 let changeQuantity = productLocalStorage [k].productQuantity;
 let qttModifValue = qttModif[k].valueAsNumber;
 
-const resultFinal = productLocalStorage.final ((el) => el.qttModifValue !== changeQuantity);
-resultSearch.productQuantity = qttModifValue;
-productLocalStorage [k].productQuantity = resultSearch.productQuantity;
+const resultFind = productLocalStorage.find ((el) => el.qttModifValue !== changeQuantity);
+resultFind.productQuantity = qttModifValue;
+productLocalStorage [k].productQuantity = resultFind.productQuantity;
 
-localStorage.setItem ("produits", JSON.stringify (productLocalStorage));
+localStorage.setItem ("produit", JSON.stringify (productLocalStorage));
 
 // actualisation
 location.reload();
         })
     }
 }
-
+changeQtt ();
 // Suppression d'un produit
+function productDelete (){
+    let btn_supprimer = document.querySelectorAll (".deleteItem");
+    for(let j = 0; j<btn_supprimer.length; j++) {
+        btn_supprimer[j].addEventListener("clic", (Event) => {
+            Event.preventDefault ();
+// élément à supprimer suivant son ID et sa couleur
+let idDelete = productLocalStorage[j].idProduct;
+let colorDelete = productLocalStorage[j].colorProduct;
+
+productLocalStorage = productLocalStorage.filter (el => el.idProduct !== idDelete || el.colorProduct !== colorDelete);
+localStorage.setItem ("produit", JSON.stringify(productLocalStorage));
+
+// Alerte sur un produit supprimé + actualisation
+alert ("produit supprimé");
+location.reload();
+        })
+    }
+}
+
+productSuppr ();
+
+// formulaire
+function getForm(){
+// Mise en place des Regex 
+let form = document.querySelector (".cart_order_form");
+
+// Mise en place des expressions régulières
+let emailRegExp = new RegExp ('^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[az]{2,10}$');
+let charRegExp = new RegExp ("^[a-zA-Z,.'-]+$");
+let addressRegExp = new RegExp ("^[0-9]{1,3}(?:(?:[,.]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)+" );
+
+// Ecoute de la modification du prénom
+form.prénom.addEventListener ("modifier", function(){
+    validFirstName (this);
+});
+
+// Ecoute de la modification du nom
+form.nom.addEventListener ("modifier", function(){
+    validLastName (this);
+});
+
+// Ecoute de la modification de l'adresse
+form.adresse.addEventListener ("modifier", function () {
+    validAddress (this);
+});
+
+// Ecoute de la modification de la ville
+form.ville.addEventListener ("modifier", function(){
+    validCity (this);
+});
+
+// Ecoute de la modification de l'email
+form.courriel.addEventListener ("modifier", function(){
+    validEmail (this);
+});
+
+// Validation du prénom
+const validFirstName = function (inputFirstName){
+    let firstNameErrorMsg = inputFirstName.nextElementSibling;
+
+    if (charRegExp.test (inputFirstName.value)){
+        firstNameErrorMsg.innerHTML = '';
+    }else{
+        firstNameErrorMsg.innerHTML = 'Renseignez ce champ';
+    }
+    };
+
+
+// Validation du nom 
+const validLastName = function(inputLastName){
+    let lastNameErrorMsg = inputLastName.nextElementSibling;
+
+    if(charRegExp.test (inputLastName.value)){
+        lastNameErrorMsg.innerHTML = '';
+    }else{
+        lastNameErrorMsg.innerHTML = 'Renseignez ce champ';
+    }
+    };
+
+// Validation de l'adresse
+const validAddress = function (inputAddress) {
+    let addressErrorMsg = inputAddress.nextElementSibling;
+
+    if(addressRegExp.test (inputAddress.value)){
+        addressErrorMsg.innerHTML = '';
+    }else{
+        addressErrorMsg.innerHTML = 'Renseigner ce champ';
+    }
+    };
+
+    // Validation de la ville 
+    const validCity = function (inputCity) {
+        let cityErrorMsg = inputCity.nextElementSibling;
+
+        if (charRegExp.test (inputCity.value)){
+            cityErrorMsg.innerHTML = '';
+        }else{
+            cityErrorMsg.innerHTML = 'Renseignez ce champ';
+        }
+        };
+
+    // Validation de l'email
+    const validEmail = function (inputEmail) {
+        let emailErrorMsg = inputEmail.nextElementSibling;
+
+        if (emailRegExp.test (inputEmail.value)) {
+            emailErrorMsg.innerHTML = '';
+        }else{
+            emailErrorMsg.innerHTML = 'Renseignez ce champ';
+        }
+        };
+    
+    }
+getForm ();
+
+// Envoi des infos client au localStorage
+
+function postForm (){
+    const btn_commander = document.getElementById ("commander");
+
+// Ecoute du panier
+btn_commander.addEventListener ("clic", (Event) => {
+
+// Récupération du formulaire client
+let inputFirstName = document.getElementById ("prénom");
+let inputLastName = document.getElementById ("nom");
+let inputAddress = document.getElementById ("adresse");
+let inputCity = document.getElementById ("ville");
+let inputEmail = document.getElementById ("email");
+
+// Construction du tableau avec le LocalStorage
+let idProducts = [];
+for (let i=0; i< productLocalStorage.length; i++) {
+    idProducts.push (productLocalStorage [i].idProduit)
+}
+console.log (idProducts);
+
+const order = {
+contacter : {
+    firstName : inputFirstName.value,
+    lastName : inputLastName.value,
+    addess : inputAddress.value,
+    city : inputCity.value,
+    email: inputEmail.value
+},
+produits : idProducts,
+}
+const options = {
+    method : 'POST',
+    body : JSON.stringify(order),
+    Headers : {
+        'Accepter': 'application/json',
+        'Type de contenu': "application/json"
+    },
+};
+
+fetch ( "http://localhost:3000/api/products/order", options)
+.then ((response) => response.json ())
+.then ((data) => {
+    console.log (data);
+    localStorage. errase ();
+    localStorage.setItem ("orderId", data.orderId);
+
+    document.location.href = "confirmation.html";
+});
+})
+}
+postForm ();
+
+
+
+
+
+
--- front/js/index.js
+++ front/js/index.js
@@ -1,75 +1,23 @@
-allProducts ();
+//----------- main function -------------// 
+(async () => {
+  const canapes = await getArticles();
+})();
 
-function allProducts(){
-  getArticles();
+//---- subfunction : first, get the articles-----//
+function getArticles() {
+  return fetch("http://localhost:3000/api/products")
+    .then(function (response) {
+      console.log("response :" + response);
+      console.log(response);
+      return response.json();
+    })
+    .then(function (responseJson) {
+      console.log("article :" +responseJson);
+      console.log(responseJson);
+      return responseJson;
+    })
+    .catch(function (error) {
+      alert(error);
+    });
 }
 
-// Récupération de la liste Produits avec l'API
-function getArticles () {
-fetch ("http://localhost:3000/api/products")
-.then(function (res){
-return res.json ();
-})
-.catch ((error) => {
-  let productsContainer = document.querySelector(".items");
-  items.innerHTML = 
-  "Oups ! Vérifiez votre connexion ...";
-  items.style.textAlign = "center";
-  items.style.width = "38vh 0";
-})
-
-// Mise en place des données de l'API dans le DOM
-async function getProducts () {
-  var result = await getArticles ()
-  .then (function (resultAPI) {
-    const articles = resultAPI;
-    console.log(articles);
-    for(let article in articles)
-  {
-// élément "a"
-let productLink = document.createElement ("a");
-document.querySelector(".items") .appendChild (productLink);
-productLink.href = `product.html?id=${resultAPI [article]. _id}`;
-
-// élément "article"
-let productArticle = document.createElement ("article");
-productLink.appendChild (productArticle);
-
-// élément "image"
-let productImg = document.createElement ("img");
-productArticle.appendChild(productImg);
-productImg.src= resultAPI [article].imageUrl;
-productImg.alt=resultAPI[article].altTxt;
-
-// élément "h3"
-let productName = document.createElement("h3");
-productArticle.appendChild(productName);
-productName.classList.add("nom_produit");
-productName.innerHTML = resultAPI[article].name;
-
-// élément "p"
-let productDescription = document.createElement ("p");
-productArticle.appendChild (productDescription);
-productDescription.classList.add("nom_produit");
-productDescription.innerHTML=resultAPI[article].description;
-
-// élément "price"
-let productPrice = document.createElement ("div");
-productInfosDiv.appendChild (productPrice);
-productPrice.classList.add("product_infos_price");
-
-// Formatage prix en euro
-resultAPI [article].prix= resultAPI[article].prix/100;
-productPrice.innerHTML = new Intl.NumberFormat ("fr-FR", {
- style : "monnaie",
-  devise : "EUR",
-}).format(resultAPI[article].prix);
-  }
-})
-.catch (function(error){
-return error;
-});
-console.log("Les produits sont importés avec succès !");
-}
-
-}
